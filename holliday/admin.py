27a876deb3a21417f08b8d6ed03e33ee530d76d8
Paula Masson Marangon <56054345+pmarangon@users.noreply.github.com>
2023-04-16 18:37:16 -0300

    Update admin.py
    Adciona tabela Holliday  no admin

--- holliday/admin.py
+++ holliday/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from holliday.models import Holliday
 
-# Register your models here.
+class HollidayAdmin(admin.ModelAdmin):
+  list_display = ('nome','dia','mes')
+  search_fields = ('dia',)
+
+admin.site.register(Holliday, HollidayAdmin)
