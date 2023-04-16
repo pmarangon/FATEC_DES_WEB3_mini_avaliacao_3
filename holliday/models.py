7fc6c318b8ff233d10b6f92c4eda52c951cd32eb
Paula Masson Marangon <56054345+pmarangon@users.noreply.github.com>
2023-04-16 17:21:07 -0300

    Update models.py
    Adciona tabela de feriados

--- holliday/models.py
+++ holliday/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Holliday(models.Model):
+  id = models.AutoField(primary_key=True)
+  celebration = models.CharField(max_length=200)
+  date = models.DateField
